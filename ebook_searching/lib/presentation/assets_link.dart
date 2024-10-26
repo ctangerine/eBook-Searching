e159188f30e62f2fb013418d6343ee7383c2aa54
Makememyday <congthinhluong.new@gmail.com>
2024-10-26 22:02:08 +0700

    add: signin screen, UI only

---
+++ ebook_searching/lib/presentation/assets_link.dart
@@ -0,0 +1,6 @@
+const String booktudLogo = 'assets/images/splash_logo.png';
+const String signinImage = 'assets/images/signin_image.png';
+const String homepageImage = 'assets/images/homepage_image.png';
+const String bookCover1 = 'assets/images/book_cover_1.png';
+const String bookCover2 = 'assets/images/book_cover_2.png';
+const String bookCover3 = 'assets/images/book_cover_3.png';
---
+++ ebook_searching/lib/presentation/screens/signin_screen.dart
@@ -0,0 +1,125 @@
+import 'package:ebook_searching/presentation/assets_link.dart';
+import 'package:ebook_searching/presentation/themes/themes.dart';
+import 'package:flutter/material.dart';
+
+class SigninScreen extends StatelessWidget {
+  const SigninScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.fromLTRB(10, 30, 10, 10),
+          child: Center(
+            child: Column(
+              children: [
+                _buildHeroImage(context),
+                const SizedBox(height: 30),
+                _buildGreetings(),
+                const SizedBox(height: 40),
+                _buildSigninLinkButton(context),
+                const SizedBox(height: 10),
+                _buildRegisterLinkButton(context),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildHeroImage(Bui) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Stack(
+          clipBehavior: Clip.none,
+          children: [
+            Image.asset(signinImage), // Ảnh của bạn
+            Positioned.fill(
+              left: -10,
+              right: -10,
+              child: Align(
+                alignment: Alignment.bottomCenter,
+                child: Container(
+                  width: double.infinity,
+                  height: 170,  // 1/3 chiều cao của ảnh
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.bottomCenter,
+                      end: Alignment.topCenter,
+                      colors: [
+                        const Color.fromARGB(255, 255, 255, 255), 
+                        const Color.fromARGB(255, 255, 255, 255).withOpacity(0.8),
+                        const Color.fromARGB(255, 255, 255, 255).withOpacity(0),           // Trong suốt ở trên
+                      ],
+                      stops: const [0.0,0.5, 1.0],  // Vị trí gradient thay đổi
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Widget _buildGreetings() {
+    return Column(
+      children: [
+        const SizedBox(
+          width: 360,
+          child: Text(
+            'Borrow library books\neasily and quickly!',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontFamily: 'Inter',
+              fontSize: 24,
+              fontWeight: FontWeight.w600,
+              letterSpacing: 0.5,
+            ),
+          ),
+        ),
+        const SizedBox(height: 10),
+        SizedBox(
+          width: 360,
+          child: Text(
+            'Borrow library books easily and conveniently, with\nquick access anytime, anywhere.',
+            textAlign: TextAlign.center,
+            style: AppTextStyles.description2Regular.copyWith(
+              color: AppColors.textSecondary,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildSigninLinkButton(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      height: 40,
+      child: FilledButton(
+        onPressed: () {
+          // navigate to signin screen
+        },
+        child: const Text('Get started'),
+      ),
+    );
+  }
+
+  Widget _buildRegisterLinkButton(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      height: 40,
+      child: OutlinedButton(
+        onPressed: () {
+          // navigate to register screen
+        },
+        child: const Text('I\'m new, sign me up'),
+      ),
+    );
+  }
+}
